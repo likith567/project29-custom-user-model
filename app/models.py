7f795221068b9c951f3a5108ee0167c5a18dcf6b
likithnani <likithnani567@gmail.com>
2021-10-19 11:20:09 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self,email,first_name,last_name,password=None):
+        if not email:
+            raise ValueError('please provide email')
+        email=self.normalize_email(email)
+        user=self.model(email=email,first_name=first_name,last_name=last_name)
+        user.set_password(password)
+        user.save()
+        return user
+    def create_superuser(self,email,first_name,last_name,password):
+        user=self.create_user(email,first_name,last_name,password)
+        user.is_staff=True
+        user.is_superuser=True
+        user.save()
+
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    email=models.EmailField(unique=True,max_length=100)
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['first_name','last_name']
+    objects=UserProfileManager()
+    def __str__(self):
+        return self.first_name+' '+self.last_name
